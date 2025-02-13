958b318034791e224379cbf574b9511da70efafa
karataev17 <dkarataev2000@gmail.com>
2025-02-13 16:58:45 +0500

    Добавлен перевод( не весь)

--- bizdin_et/urls.py
+++ bizdin_et/urls.py
@@ -5,3 +5,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reports.urls')),  # Подключаем API
 ]
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns += [
+    path('i18n/', include('django.conf.urls.i18n')),  # Маршрут для смены языка
+]
+
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('reports.urls')),  # Основное приложение
+)
